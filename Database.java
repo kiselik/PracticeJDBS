73472514211861556935d21ab207f6025f1d0e45
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-04-05 00:53:07 +0300

    it's work

--- Database.java
+++ Database.java
@@ -5,21 +5,16 @@ public class Database {
 
     Database() throws ClassNotFoundException, SQLException {
         String driver = "oracle.jdbc.OracleDriver";
-        //Class.forName("oracle.jdbc.driver.OracleDriver");
-        //DriverManager.getDriver(driver);
-        //DriverManager.registerDriver(DriverManager.getDriver(driver));
         Class.forName("oracle.jdbc.OracleDriver");
         conn = DriverManager.getConnection("jdbc:oracle:thin:@sql.edu-netcracker.com:1251:XE",
                 "unc17i_kiseleva", "pp6pj9VN");
-
-        //conn.close( );
     }
 
     String searchEmployee(int id) throws ClassNotFoundException, SQLException {
         PreparedStatement stat = conn.prepareStatement("SELECT e.ename AS NAME,e.job,m.mg,e.hiredate,e.sal, d.dname AS DEPART,d.loc AS LOCATION " +
-                                            "FROM dept d,emp e, " +
-                                            "(SELECT e1.ename,e2.ename AS mg FROM emp e1,emp e2 WHERE e2.empno=e1.mgr) m " +
-                                            " WHERE e.empno=? AND d.deptno=e.deptno AND m.ename=e.ename");
+                "FROM dept d,emp e, " +
+                "(SELECT e1.ename,e2.ename AS mg FROM emp e1,emp e2 WHERE e2.empno=e1.mgr) m " +
+                " WHERE e.empno=? AND d.deptno=e.deptno AND m.ename=e.ename");
         stat.setInt(1, id);
 
         ResultSet res = stat.executeQuery();
@@ -29,16 +24,102 @@ public class Database {
 
         while (res.next()) {
             for (int i = 1; i <= size; i++) {
-                tmp.append(res.getMetaData().getColumnName(i)+": ");
+                tmp.append(res.getMetaData().getColumnName(i) + ": ");
                 tmp.append(res.getString(i));
                 tmp.append(" ");
             }
         }
+        stat.close();
         if (tmp.toString().isEmpty())
-            return  "Well, i haven't this id of Employee";
+            return "Well, i haven't this id of Employee";
         else
-        return tmp.toString();
+            return tmp.toString();
     }
+    String searchEmployeeJob(String dept) throws ClassNotFoundException, SQLException {
+        PreparedStatement stat = conn.prepareStatement("SELECT e.ename AS NAME,m.mg,e.hiredate,e.sal, d.dname AS DEPART,d.loc AS LOCATION " +
+                "FROM dept d,emp e, " +
+                "(SELECT e1.ename,e2.ename AS mg FROM emp e1,emp e2 WHERE e2.empno=e1.mgr) m " +
+                " WHERE e.job=? AND d.deptno=e.deptno AND m.ename=e.ename");
+        stat.setString(1, dept);
+
+        ResultSet res = stat.executeQuery();
+        int size = res.getMetaData().getColumnCount();
+        StringBuilder tmp = new StringBuilder();
+
+
+        while (res.next()) {
+            for (int i = 1; i <= size; i++) {
+                tmp.append(res.getMetaData().getColumnName(i) + ": ");
+                tmp.append(res.getString(i));
+                tmp.append(" ");
+            }
+        }
+        stat.close();
+        if (tmp.toString().isEmpty())
+            return "Well, i haven't this id of Employee";
+        else
+            return tmp.toString();
+    }
+
+    String searchEmployeeDept(String job) throws ClassNotFoundException, SQLException {
+        PreparedStatement stat = conn.prepareStatement("SELECT e.ename AS NAME,e.job,m.mg,e.hiredate,e.sal AS DEPART,d.loc AS LOCATION " +
+                "FROM dept d,emp e, " +
+                "(SELECT e1.ename,e2.ename AS mg FROM emp e1,emp e2 WHERE e2.empno=e1.mgr) m " +
+                " WHERE d.dname=? AND d.deptno=e.deptno AND m.ename=e.ename");
+        stat.setString(1, job);
+
+        ResultSet res = stat.executeQuery();
+        int size = res.getMetaData().getColumnCount();
+        StringBuilder tmp = new StringBuilder();
+
+
+        while (res.next()) {
+            for (int i = 1; i <= size; i++) {
+                tmp.append(res.getMetaData().getColumnName(i) + ": ");
+                tmp.append(res.getString(i));
+                tmp.append(" ");
+            }
+        }
+        stat.close();
+        if (tmp.toString().isEmpty())
+            return "Well, i haven't this id of Employee";
+        else
+            return tmp.toString();
+    }
+
+    void AddEmployee(Integer empno, String ename, String job, Integer mgr, String hiredate, Integer salary, String comm, Integer deptno) throws ClassNotFoundException, SQLException {
+        PreparedStatement stats = conn.prepareStatement("INSERT INTO emp VALUES (?,?,?,?,TO_DATE(?, 'DD-MM-YYYY'),?,?,?)");
+        stats.setString(1, empno.toString());
+        stats.setString(2, ename);
+        stats.setString(3, job);
+        stats.setString(4, mgr.toString());
+        stats.setString(5, hiredate);
+        stats.setString(6, salary.toString());
+        stats.setString(7, comm);
+        stats.setString(8, deptno.toString());
+        int res = stats.executeUpdate();
+        if (res != 0) {
+            System.out.println("1 row inserted");
+            conn.commit();
+        } else
+            System.out.println("Well, i haven't this id of Employee");
+        stats.close();
+
+    }
+
+    void DeleteEmployee(int id) throws ClassNotFoundException, SQLException {
+        PreparedStatement stat = conn.prepareStatement("DELETE FROM emp e WHERE e.empno=?");
+        stat.setInt(1, id);
+
+        int res = stat.executeUpdate();
+        if (res != 0) {
+            System.out.println("1 row deleted");
+            conn.commit();
+        } else
+            System.out.println("Well, i haven't this id of Employee");
+        stat.close();
+    }
+
 
     void closeConn() throws ClassNotFoundException, SQLException {
         conn.close();
--- Menu.java
+++ Menu.java
@@ -14,11 +14,13 @@ public class Menu {
     }
 
     public void run() throws ClassNotFoundException, SQLException {
-        while (selection != 4) {
+        while (selection != 6) {
             System.out.println("[1]  Find Employee");
-            System.out.println("[2]  Add Employee");
-            System.out.println("[3]  Delete Employee");
-            System.out.println("[4]  Quit");
+            System.out.println("[2]  Find Employees for job");
+            System.out.println("[3]  Find Employees for departs");
+            System.out.println("[4]  Add Employee");
+            System.out.println("[5]  Delete Employee");
+            System.out.println("[6]  Quit");
             System.out.print("Write number there: ");
 
             Scanner reader = new Scanner(System.in);
@@ -31,32 +33,87 @@ public class Menu {
                 case 1:
                     System.out.println("------------------------------------------");
                     System.out.println("Please, write id Employee there");
-                    int id;
                     if (reader.hasNextInt()) {
-                        id = reader.nextInt();
+                        int id = reader.nextInt();
                         System.out.println("Are you sure? Y/N");
                         if (reader.next().equals("Y")) {
                             dat = new Database();
                             System.out.println(dat.searchEmployee(id));
                             dat.closeConn();
                         }
-
                     } else
                         System.out.println("Well, it isn't a number.Try again");
                     System.out.println("------------------------------------------");
                     break;
                 case 2:
                     System.out.println("------------------------------------------");
-                    System.out.println("2");
+                    System.out.println("Please, write job of Employee there");
+                    if (reader.hasNextLine()) {
+                        reader.nextLine();
+                        String job = reader.nextLine();
+                        System.out.println("Are you sure? Y/N");
+                        if (reader.next().equals("Y")) {
+                            dat = new Database();
+                            System.out.println(dat.searchEmployeeJob(job));
+                            dat.closeConn();
+                        }
+                    } else
+                        System.out.println("Well, it isn't a string.Try again");
                     System.out.println("------------------------------------------");
                     break;
                 case 3:
                     System.out.println("------------------------------------------");
-                    System.out.println("3");
+                    System.out.println("Please, write name of Employees depart there");
+                    if (reader.hasNextLine()) {
+                        reader.nextLine();
+                        String dept = reader.nextLine();
+                        System.out.println("Are you sure? Y/N");
+                        if (reader.next().equals("Y")) {
+                            dat = new Database();
+                            System.out.println(dat.searchEmployeeDept(dept));
+                            dat.closeConn();
+                        }
+                    } else
+                        System.out.println("Well, it isn't a string.Try again");
                     System.out.println("------------------------------------------");
                     break;
                 case 4:
-                    System.out.println("4");
+                    System.out.println("------------------------------------------");
+                    System.out.println("Please, write information about Employee in format");
+                    System.out.println("[id employee][name][position][number of manager][hiredate DD-MM-YYYY][salary][comm][number of department]");
+                    System.out.println("example:  5 olga princess 7499 12-09-2013 4600 50 20");
+                    reader.nextLine();
+                    String tmp = reader.nextLine();
+                    System.out.println("Are you sure? Y/N");
+                    if (reader.next().equals("Y")) {
+                        String[] inf = tmp.split(" ");
+                        dat = new Database();
+                        try {
+                            dat.AddEmployee(Integer.parseInt(inf[0]), inf[1], inf[2], Integer.parseInt(inf[3]), inf[4], Integer.parseInt(inf[5]), inf[6], Integer.parseInt(inf[7]));
+                        } catch (SQLException e) {
+                            System.out.println("Oops, you have wrong option in centence");
+                        }
+                        dat.closeConn();
+                    }
+                    System.out.println("------------------------------------------");
+                    break;
+                case 5:
+                    System.out.println("------------------------------------------");
+                    System.out.println("Please, write id Employee there");
+                    int id_emp;
+                    if (reader.hasNextInt()) {
+                        id_emp = reader.nextInt();
+                        System.out.println("Are you sure? Y/N");
+                        if (reader.next().equals("Y")) {
+                            dat = new Database();
+                            dat.DeleteEmployee(id_emp);
+                            dat.closeConn();
+                        }
+                    } else
+                        System.out.println("Well, it isn't a number.Try again");
+                    System.out.println("------------------------------------------");
+                    break;
+                case 6:
                     break;
                 default:
                     System.out.println("Unfortunately, I don't understand your choice =(");
